835dcdfe8f7109af49a94ee1efeb1ea21cf6fd82
Monzurul Islam <Xatta-Trone@users.noreply.github.com>
2022-02-26 22:53:10 +0900

    asdf

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -30,6 +30,11 @@
 
 <hr>
 
+<hr>
+@if (!auth()->user()->t_chat_id)
+<script async src="https://telegram.org/js/telegram-widget.js?15" data-telegram-login="xattabot" data-size="large"
+    data-auth-url="/tbot" data-request-access="write"></script>
+@else
 <form action="sendmsg" method="GET">
     @csrf
     <label for="sendmsg">Send a msg to yourself</label><br>
@@ -37,12 +42,6 @@
 <p>try sending an empty message!!!</p>
     <input type="submit" value="send" />
 </form>
-
-
-<hr>
-@if (!auth()->user()->t_chat_id)
-<script async src="https://telegram.org/js/telegram-widget.js?15" data-telegram-login="xattabot" data-size="large"
-    data-auth-url="/tbot" data-request-access="write"></script>
 @endif
 
 
